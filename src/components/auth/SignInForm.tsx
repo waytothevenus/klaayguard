f79153e65a452df3f6087a3a80096486c99cdd38
waytothevenus <andyhung772@gmail.com>
2025-04-30 11:58:19 +0300

    fix(frontend): fix UI issue in signin page

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -60,14 +60,14 @@ export default function SignInForm() {
   };
 
   return (
-    <div className="flex flex-col flex-1">
+    <div className="flex flex-col flex-1 items-center px-8">
       <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
         <div>
           <div className="mb-5 sm:mb-8">
-            <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
+            <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md text-center">
               Sign In
             </h1>
-            <p className="text-sm text-gray-500 dark:text-gray-400">
+            <p className="text-sm text-gray-500 dark:text-gray-400 text-center">
               Enter your email and password to sign in!
             </p>
           </div>
@@ -99,7 +99,7 @@ export default function SignInForm() {
                       placeholder="Enter your password"
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
-                        error={!!passwordError}
+                      error={!!passwordError}
                     />
                     <span
                       onClick={() => setShowPassword(!showPassword)}
@@ -112,7 +112,9 @@ export default function SignInForm() {
                       )}
                     </span>
                     {passwordError && (
-                      <p className="text-error-500 text-sm mt-1">{passwordError}</p>
+                      <p className="text-error-500 text-sm mt-1">
+                        {passwordError}
+                      </p>
                     )}
                   </div>
                 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
-export default defineConfig({
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST || "0.0.0.0";
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
   plugins: [
     react(),
+    // tailwindcss(),
     svgr({
       svgrOptions: {
         icon: true,
@@ -15,4 +20,26 @@ export default defineConfig({
       },
     }),
   ],
-});
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
